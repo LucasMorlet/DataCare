3559f60b44705ab83680c44ff45373397b3e0c4c
Lucas Morlet <62259047+LucasMorlet@users.noreply.github.com>
2021-04-10 10:48:01 +0200

    ça marche enfin

--- test_read_xml.py
+++ test_read_xml.py
@@ -12,8 +12,9 @@ for event, elem in xml_iter :
         
 import xml.dom.minidom
  
-xml = xml.dom.minidom.parse("test.xml")
-liste = xml.getElementsByTagName("food")
+doc_xml = xml.dom.minidom.parse("test.xml")
+root = doc_xml.documentElement
+liste = root.getElementsByTagName("food")
 print ( "On a", len (liste), "element" )
 for element in liste :
-    print ( element.getAttribute("price") )
+    print ( element.getElementsByTagName("name")[0].firstChild.nodeValue )
