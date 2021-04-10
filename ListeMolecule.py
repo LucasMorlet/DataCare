8e7fe5af0d1a9121cc8a148eae525e0bdfdc4fe0
Lucas Morlet <62259047+LucasMorlet@users.noreply.github.com>
2021-04-10 07:31:01 +0200

    Initial commit

---
+++ ListeMolecule.py
@@ -0,0 +1,44 @@
+from Molecule import *
+from datetime import datetime
+
+class ListeMolecule :
+
+    def __init__ ( self, n ) :
+        self.nom = n
+        self.liste = []
+        
+    def __str__ ( self ) :
+        s = "--------------- " + self.nom + " ---------------\n\n"
+        for i in range ( len ( self.liste ) ) :
+            s += str ( self.liste[i] ) + "\n"
+            
+        return s
+        
+    # Ajouter une molécule à la liste
+    # TODO : changer la structure de données vers une table de hachage
+    def addMolecule ( self, m ) :
+        self.liste.append(m)
+        
+    # Ecriture dans un fichier
+    def print_in_file ( self ) :
+        filename = "Resultats\\" + self.nom + "_" + datetime.now().strftime("%Y_%m_%d_%Hh%M") + ".txt"
+        file = open(filename, "x")
+        file.write ( str ( self ) )
+        file.close()
+        
+# Méthode de test       
+def testListe ( ) :
+    L = ListeMolecule ( "Molecule_test" )
+    m = Molecule()
+    m.setNom ( "Alice" )
+    m.setSmile ( "Smile Alice" )
+    L.addMolecule ( m )
+    m2 = Molecule()
+    m2.setNom ( "Bob" )
+    m2.setSmile ( "Smile Bob" )
+    L.addMolecule ( m2 )
+    print(L)
+    L.print_in_file()
+    
+    
+testListe()
---
+++ Molecule.py
@@ -0,0 +1,33 @@
+class Molecule :
+
+    # Constructeur
+    def __init__ ( self ) :
+        self.nom = None
+        self.smile = None
+       
+    # toString
+    def __str__ ( self ) :
+        s = ""
+        s += "##### " + self.nom + " #####\n"
+        s += self.smile + "\n"
+        return s
+    
+    # Accesseur
+    def setNom ( self, n ) :
+        self.nom = n
+        
+    def setSmile ( self, s ) :
+        self.smile = s
+        
+        
+        
+def testMolecule ( n, s ) :
+    m = Molecule()
+    m.setNom ( n )
+    m.setSmile ( s )
+    print ( m )
+    
+'''   
+testMolecule ( "Alice", "Smile 1" )
+testMolecule ( "Bob", "Smile 2" )
+'''
