fea6a9498d8bbfc710d3a935ecaa3840f169ac5e
Lucas Morlet <62259047+LucasMorlet@users.noreply.github.com>
2021-04-10 15:56:00 +0200

    travail à faire

--- test_read_xml.py
+++ test_read_xml.py
@@ -9,3 +9,14 @@ liste = root.getElementsByTagName("drug")
 print ( "On a", len (liste), "element" )
 for element in liste :
     print ( element.getElementsByTagName("name")[0].firstChild.nodeValue )
+    
+    
+    
+# Récupération du contenu du dossier (pour chaque fichier)
+def traite_dossier ():
+    None
+
+
+# Extrait l'id et créer un fichier de ce nom dans le dossier Rename
+def range_fichier () :
+    None
