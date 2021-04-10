18811577a63b32598a098c605fa5bb2b59de411f
Lucas Morlet <62259047+LucasMorlet@users.noreply.github.com>
2021-04-10 14:39:49 +0200

    premier test sur la vraie BDD

--- test_read_xml.py
+++ test_read_xml.py
@@ -1,20 +1,11 @@
-'''from xml.etree import ElementTree
-
-xml_iter = ElementTree.iterparse("test.xml", events=('start', 'end'))
-
-for event, elem in xml_iter :
-    if event == 'start' :
-        string = "<" + elem.tag + ">\n"
-        string += elem.text.strip() +"\n"
-    elif event == 'end' :
-        string = "</" + elem.tag + ">\n"
-        print ( string )'''
-        
 import xml.dom.minidom
- 
-doc_xml = xml.dom.minidom.parse("test.xml")
+
+# Récupération du doc XML
+doc_xml = xml.dom.minidom.parse("../DataBase/database.xml")
 root = doc_xml.documentElement
-liste = root.getElementsByTagName("food")
+
+
+liste = root.getElementsByTagName("drug")
 print ( "On a", len (liste), "element" )
 for element in liste :
     print ( element.getElementsByTagName("name")[0].firstChild.nodeValue )
