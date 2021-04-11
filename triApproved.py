58b2d53851c6c4461b1866b2c3c7316d47d9d377
Lucas Morlet <62259047+LucasMorlet@users.noreply.github.com>
2021-04-11 15:01:39 +0200

    Passage en CSV

--- triApproved.py
+++ triApproved.py
@@ -2,10 +2,11 @@ import os
 import shutil
 import xml.dom.minidom
 
-fichierTrie=""
 caractere_de_separation="\\"
+caractere_csv = ';'
 dossier_lecture = "DataBase\\Rename"
 dossier_ecriture = "DataBase\\Rename2"
+
 
 def traiter_doc_txt(filename):
     try :
@@ -26,25 +27,41 @@ def traiter_doc_txt(filename):
         else :  print("\n\n[ERROR!] Attention, le fichier doc {} n'existe pas\n\n".format(filename))
         return -1
  
-def traiter_fichier_unique_approved(filename):
+def traiter_fichier_unique_approved_commercial(filename):
     
     try :
         path_lecture= dossier_lecture + caractere_de_separation + filename+".xml"
         with open(path_lecture,"r") as file :
 
+            # Racine du XML
             doc_xml = xml.dom.minidom.parse(file)
             root = doc_xml.documentElement
 
+            # Etat : approved/unknow/other
             group = root.getElementsByTagName("group")
             result=[]
-            # parcours le tag "group"
+            etat = "unknown"
             for i in range( len( group ) ):
                 result.append(group[i].firstChild.nodeValue)
             for i in range (len(result)) :
+                etat = "other"
                 elem=result[i]
                 if elem =="approved":
-                    copie_fichiers(filename)
+                    etat = "approved"
+                    break
+                    
+            # Commercial/Non Commercial
+            commercial = "commercial"
+            # TODO marketing ?
 
+            
+            resultat = ""
+            resultat += filename 
+            resultat += caractere_csv 
+            resultat += etat 
+            resultat += caractere_csv
+            resultat += commercial
+            return resultat
             
     except :
         if type(filename) != str : print("[ERROR!] Attention, filename = {} n'est pas une variable string".format(filename))
@@ -54,7 +71,8 @@ def traiter_fichier_unique_approved(filename):
 def juge_fichiers(filename):
     liste_identifiants=traiter_doc_txt(filename)
     for i in range(len(liste_identifiants)):
-        traiter_fichier_unique_approved(liste_identifiants[i])
+        res = traiter_fichier_unique_approved_commercial(liste_identifiants[i])
+        print ( res )
 
 def copie_fichiers(nomDepart):
     path_ecriture= dossier_ecriture + caractere_de_separation + nomDepart + ".xml"
