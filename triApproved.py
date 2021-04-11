072b41d20bc342174e960e315f7e8018cf12d45a
Lucas Morlet <62259047+LucasMorlet@users.noreply.github.com>
2021-04-11 16:44:25 +0200

    test csv

--- triApproved.py
+++ triApproved.py
@@ -1,12 +1,13 @@
 import os
 import shutil
 import xml.dom.minidom
+from datetime import datetime
 
 caractere_de_separation="\\"
 caractere_csv = ';'
-dossier_lecture = "DataBase\\Rename"
-dossier_ecriture = "DataBase\\Rename2"
-
+dossier_lecture = "DataBase" + caractere_de_separation + "Rename"
+dossier_ecriture = "DataBase" + caractere_de_separation + "Rename2"
+csv_path = "Resultats" + caractere_de_separation
 
 def traiter_doc_txt(filename):
     try :
@@ -69,10 +70,15 @@ def traiter_fichier_unique_approved_commercial(filename):
         return -1
 
 def juge_fichiers(filename):
+    csv = create_csv ( filename )
+    if csv == None :
+        return
     liste_identifiants=traiter_doc_txt(filename)
     for i in range(len(liste_identifiants)):
         res = traiter_fichier_unique_approved_commercial(liste_identifiants[i])
-        print ( res )
+        write_in_csv ( res, csv )
+        
+    print ( extract_from_csv ( csv, True, False, False, True, True ) )
 
 def copie_fichiers(nomDepart):
     path_ecriture= dossier_ecriture + caractere_de_separation + nomDepart + ".xml"
@@ -89,12 +95,65 @@ def copie_fichiers(nomDepart):
                     else:
                         continu = False
     except :
-        print ( "Le fichier d'arrivée n'est aps bon" )
+        print ( "Le fichier d'arrivée n'est pas bon" )
         return -1
+        
+def create_csv ( entree ) :
+    filename = csv_path + entree + "_" + datetime.now().strftime("%Y_%m_%d_%Hh%Mm%Ss") + ".csv"
+    try :
+        fichier = open( filename, 'x' )
+        fichier.close()
+        return filename
+    except :
+        print ( "Impossible de créer le fichier CSV :", filename )
+        return None
             
 
-        
-        
+def write_in_csv ( ligne, csv ) :
+    try :
+        fichier = open( csv, 'a' )
+        fichier.write ( ligne + "\n" )
+        fichier.close()
+    except :
+        print ( "Fail : Impossible d'écrire dans ", csv )
+
+    
+def extract_from_csv ( csv, app, unk, oth, com, notcom ) :
+    data = []
+    try :
+        fichier = open( csv, 'r' )
+        continu = True
+        while (continu):
+            ligne = fichier.readline()
+            if ligne :
+                ligne = ligne.split("\n")[0]
+                data_line = ligne.split( caractere_csv )
+                db_id = data_line[0]
+                group = data_line[1]
+                comm = data_line[2]
+                
+                ok_group = False
+                if app == True and group == "approved" :
+                    ok_group = True
+                elif unk == True and group == "unknown" :
+                    ok_group = True
+                elif oth == True and group == "other" :
+                    ok_group = True
+                
+                ok_comm = False
+                if com == True and comm == "commercial" :
+                    ok_comm = True
+                elif notcom == True and comm == "not-commercial" :
+                    ok_comm = True
+                    
+                if ( ok_group and ok_comm ) :
+                    data.append ( db_id )
+                
+            else:
+                continu = False
+    except :
+        print ( "Fail : Impossible de lire dans", csv )
+    return data
         
 
 #traiter_fichier_unique_approved("database-100.xml")
